f424d7129919372268fb18afb16d5753945a0a50
ehEheH3heHeh3he <next.thanakrit@gmail.com>
2023-09-07 23:50:07 +0700

    judo

--- RPiProject/discord_test.py
+++ RPiProject/discord_test.py
@@ -8,13 +8,8 @@ table = [1148506462704910386,1148505332809732127,1148506620054229042]
         #role channel id, table 1 channel id, table 2 channel id
         
 TK = ''
-menu = {'americano': 20
-        ,'latte': 15
-        ,'matcha': 50
-        ,'croissant': 90
-        ,'waffle': 40}
 queue = []
-
+menu = ['waffle','latte','americano','matcha','chocolate']
 
 #define GPIO pins
 direction= 22 # Direction (DIR) GPIO Pin
@@ -46,6 +41,10 @@ async def on_message(message):
     if channel.id == 1148509385912500234 and message.content.startswith('move'):
         await message.channel.send('OK!')
         home_table(1)
+        return
+    elif channel.id == 1148509385912500234 and message.content.startswith('check'):
+        await message.channel.send(queue)
+        return
 
     # assing role
     if channel.id == table[0] and message.content.lower() == ('0001'): # id of role channel
@@ -61,26 +60,56 @@ async def on_message(message):
     elif channel.id == table[0]:
         await message.delete()
 
-    
     # table 1 channel
     if channel.id == table[1] and message.content.lower() == ('order'):
-        await message.channel.send('What would you like?')
-        # if channel.id == table[1] and message.content.lower() == ('matcha'):
-        #     queue.append(message.lower()+channel.id)
-        #     await message.channel.send('Order received please wait...')
+        await message.channel.send('What do you want?')
+        await message.channel.send('Menu\n1. Latte\n2. Americano\n3. Matcha\n4. Chocolate')
+        def check(m):
+            for content in menu:
+                if m.content.lower() == content and m.channel == channel:
+                    var = content
+                    queue.append(var) # add order to queue
+                    
+                    return m.content.lower() == content and m.channel == channel, queue
+                elif m.content.lower() == ('cancel') and m.channel == channel:
+                    return m.content.lower() == ('cancel') and m.channel == channel, queue
+            return queue
+        
+        msg = await client.wait_for('message', check=check)
+        await message.channel.send(f'{queue}'.format(msg)) # print queue
+        return
+    # if message.content.lower() == ('matcha'):
+    #     queue.append('table1_matcha')
+    #     await message.channel.send('Order received please wait...')
+    #     return
+    # elif message.content.lower() == ('latte'):
+    #     queue.append('table1_latte')
+    #     await message.channel.send('Order received please wait...')
+    #     return
+    # elif message.content.lower() == ('americano'):
+    #     queue.append('table1_americano')
+    #     await message.channel.send('Order received please wait...')
+    #     return
+    # elif message.content.lower() == ('chocolate'):
+    #     queue.append('table1_chocolate')
+    #     await message.channel.send('Order received please wait...')
+    #     return
+    # elif message.content.lower() == ('cancel'):
+    #     await message.channel.send('Order canceled')
+    #     return
 
-        for content in menu:
-            if message.content.lower() == content:
-                await message.channel.send(f'{content} : {menu[content]} baht')
-                msg = await client.wait_for('message', check=message, timeout=30)
-                if 'paid' in msg:
-                    queue.append(content+channel.id)
-                    print(queue)
-                    await message.channel.send('Order received please wait...')
-                    return
-                elif 'cancel' in msg:
-                    await message.channel.send('Order canceled')
-                    return
+        # for content in menu:
+        #     if message.content.lower() == content:
+        #         await message.channel.send(f'{content} : {menu[content]} baht')
+        #         msg = await client.wait_for('message', check=message, timeout=30)
+        #         if 'paid' in msg:
+        #             queue.append(content+channel.id)
+        #             print(queue)
+        #             await message.channel.send('Order received please wait...')
+        #             return
+        #         elif 'cancel' in msg:
+        #             await message.channel.send('Order canceled')
+        #             return
 
         # delete role
     elif channel.id == table[1] and message.content.lower() == ('leave'):
--- RPiProject/stepper_motor_control.py
+++ RPiProject/stepper_motor_control.py
@@ -7,37 +7,78 @@ import time
 ################################
 #
 #define GPIO pins
-direction= 22 # Direction (DIR) GPIO Pin
-step = 23 # Step GPIO Pin
-EN_pin = 24 # enable pin (LOW to enable)
+D1= 22 # Direction (DIR) GPIO Pin
+S1 = 23 # Step GPIO Pin
+E1 = 24 # enable pin (LOW to enable)
+
+D2 = 12
+S2 = 5
+E2 = 6
 
 # Declare a instance of class pass GPIO pins numbers and the motor type
-mymotortest = RpiMotorLib.A4988Nema(direction, step, (21,21,21), "DRV8825")
-GPIO.setup(EN_pin,GPIO.OUT) # set enable pin as output
+mymotortest1 = RpiMotorLib.A4988Nema(D1, S1, (21,21,21), "DRV8825")
+GPIO.setup(E1,GPIO.OUT) # set enable pin as output
+
+mymotortest2 = RpiMotorLib.A4988Nema(D2, S2, (21,21,21), "DRV8825")
+GPIO.setup(E2,GPIO.OUT) # set enable pin as output
 
 ###########################
 # Actual motor control
 ###########################
-#
+
+from threading import Thread
+
+def left_r():
+    print('left1')
+    GPIO.output(E1,GPIO.LOW) # pull enable to low to enable motor
+    mymotortest1.motor_go(False, # True=Clockwise, False=Counter-Clockwise
+                            "Full" , # Step type (Full,Half,1/4,1/8,1/16,1/32)
+                            200, # number of steps
+                            .0005, # step delay [sec]
+                            False, # True = print verbose output 
+                            .05) # initial delay [sec]
+    GPIO.output(E1,GPIO.HIGH)
+
+def left_l():
+    print("left2")
+    GPIO.output(E2,GPIO.LOW)
+    mymotortest2.motor_go(False, # True=Clockwise, False=Counter-Clockwise
+                        "Full" , # Step type (Full,Half,1/4,1/8,1/16,1/32)
+                        200, # number of steps
+                        .0005, # step delay [sec]
+                        False, # True = print verbose output 
+                        .05) # initial delay [sec]
+    GPIO.output(E2,GPIO.HIGH)
+def right_r():
+    print("right1")
+    GPIO.output(E1,GPIO.LOW) # pull enable to low to enable motor
+    mymotortest1.motor_go(True, # True=Clockwise, False=Counter-Clockwise
+                            "Full" , # Step type (Full,Half,1/4,1/8,1/16,1/32)
+                            200, # number of steps
+                            .0005, # step delay [sec]
+                            False, # True = print verbose output 
+                            .05) # initial delay [sec]
+    GPIO.output(E1,GPIO.HIGH)
+
+def right_l():
+    print("right2")
+    GPIO.output(E2,GPIO.LOW)
+    mymotortest2.motor_go(True, # True=Clockwise, False=Counter-Clockwise
+                        "Full" , # Step type (Full,Half,1/4,1/8,1/16,1/32)
+                        200, # number of steps
+                        .0005, # step delay [sec]
+                        False, # True = print verbose output 
+                        .05) # initial delay [sec]
+    GPIO.output(E2,GPIO.HIGH)
 i =0
 while True :
-    GPIO.output(EN_pin,GPIO.HIGH)
     order = input("Order: ")
     if order == "1":
-        GPIO.output(EN_pin,GPIO.LOW) # pull enable to low to enable motor
-        mymotortest.motor_go(False, # True=Clockwise, False=Counter-Clockwise
-                             "Full" , # Step type (Full,Half,1/4,1/8,1/16,1/32)
-                             200, # number of steps
-                             .0005, # step delay [sec]
-                             False, # True = print verbose output 
-                             .05) # initial delay [sec]
+        Thread(target = left_r).start()
+        Thread(target = left_l).start()
+        
     elif order == "2":
-        GPIO.output(EN_pin,GPIO.LOW) # pull enable to low to enable motor
-        mymotortest.motor_go(True, # True=Clockwise, False=Counter-Clockwise
-                             "Full" , # Step type (Full,Half,1/4,1/8,1/16,1/32)
-                             200, # number of steps
-                             .0005, # step delay [sec]
-                             False, # True = print verbose output 
-                             .05) # initial delay [sec]
-    
+        Thread(target = right_r).start()
+        Thread(target = right_l).start()
+
 GPIO.cleanup() # clear GPIO allocations after run
